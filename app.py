03c7fd9d4eda19e945ec177c9a567482562487bc
Dhrumil Vaghasiya <82659478+DhrumilVaghasiya@users.noreply.github.com>
2023-03-05 09:32:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import json
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def handle_post():
+    data = request.get_json() # assuming request body is in JSON format
+    data = data.get('queryResult').get('parameters')
+    print(data)
+    msg = ""
+    for x in data:
+        if(not data[x]=='' and not data[x]==[]):
+            msg+=x+" : "
+            if(type(data[x])==list):
+                msg+=" [ "
+                for val in data[x]:
+                    msg+=val + " "
+                msg+=" ] "
+            else:
+                msg+=data[x]
+    print(msg)
+    res = {
+        "fulfillmentText": f"{msg}",
+        "source": "My Webhook"
+    }
+    # Return the response as a JSON object
+    return jsonify(res)
+
+if __name__ == '_main_':
+    app.run(debug=True,port=3000)
